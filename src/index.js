3c137e8d950b93efd80a07f381b518cea294084a
Edgar <edgargc.upc@gmail.com>
2021-08-11 13:34:20 +0200

    code small deleted and npm package created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { average } from './services/average.js'
-export { fOneScoreMacro } from './services/fOneScore.js'
+export { fOneScoreMacro } from './services/fOneScoreMacro.js'
 export { meanSquareError } from './services/meanSquareError.js'
 export { sumArray } from './services/sumArray.js'
